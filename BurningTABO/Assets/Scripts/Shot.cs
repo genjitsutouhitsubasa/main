df25ecd92b8acdfbd005e2d61cdb3f0e2595cff5
r-hatase <dzm512.myu@gmail.com>
2016-05-21 22:45:13 +0900

    shot系の追加

---
+++ BurningTABO/Assets/Scripts/Shot.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Shot : MonoBehaviour {
+	[SerializeField]
+	private float radius; // 円判定の半径
+
+	public Vector2 vec; // 飛ぶ方向
+	public float speed;
+
+	public List<Player> players = new List<Player>();
+
+	public void Init(List<Player> p, Vector2 vec)
+	{
+		this.players = p;
+		this.vec = vec;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	 
+		this.transform.position += new Vector3((vec * speed).x, (vec * speed).y, 0);
+
+		// 当たり判定
+
+	}
+}
